bacc9af5d84da9360f5af1f475f02f98ba3652c7
KevinTokisaki <tokisakikevin@gmail.com>
2024-12-27 21:13:51 +0800

    test main menu

--- Assets/Scripts/StartCtrl.cs
+++ Assets/Scripts/StartCtrl.cs
@@ -7,12 +7,16 @@ using UnityEngine.UI;
 public class StartCtrl : MonoBehaviour
 {
     public Button btnStart;
+    public Button LevelEasy;
+    public Button BtnDifficulty;
+    public GameObject Selection;
+
     void Start()
     {
         btnStart.onClick.AddListener(()=> 
         {
             SceneManager.LoadScene("Level 1(Woo)");
         });
-    }
 
+    }
 }
